70733bfba3ff80fbf7cbfd00b7eb811d5f76752e
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-10-03 21:33:25 +0330

    scroll up and scroll header js

--- main.js
+++ main.js
@@ -15,6 +15,29 @@ navLink.forEach(link=>{
     })
 })
 
+// scroll up
+window.addEventListener("scroll", () => {
+    const scrollUpBtn = document.getElementById("scroll-up");
+    if (window.scrollY >= 250) {
+        scrollUpBtn.classList.remove("-bottom-1/2");
+        scrollUpBtn.classList.add("bottom-4");
+    } else {
+        scrollUpBtn.classList.remove("bottom-4");
+        scrollUpBtn.classList.add("-bottom-1/2");
+    }
+});
+// scroll header
+window.addEventListener("scroll", () => {
+    const header = document.getElementById("navbar");
+    if (window.scrollY >= 50) {
+        header.classList.add("border-b" , "border-yellow-500");
+    } else {
+        header.classList.remove("border-b" , "border-yellow-500");
+    }
+});
+
+
+
 
 // swiper
 const swiper = new Swiper('.swiper', {
